89cf76d21a099f316813875e2a0d2e16e45b48b4
Zahhask <tomasgaspar95@gmail.com>
2025-07-13 16:44:39 +0100

    created the class, might be wrong

--- libftpp/data_struct/inc/pool.hpp
+++ libftpp/data_struct/inc/pool.hpp
@@ -1,7 +1,21 @@
 #ifndef POOL_HPP
 #define POOL_HPP
-#include "libft.hpp"
+
+#include <iostream>
+
+template<typename T>
+class Pool{
+	private:
+		T obj;
+	public:
+		class Object{
+			public:
+					
+		};
+	void resize(const size_t& numberOfObjectsStored);
+	template
+};
 
 
 
-#endif POOL_HPP
+#endif
