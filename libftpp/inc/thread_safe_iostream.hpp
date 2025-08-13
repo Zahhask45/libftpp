d7a571e5fe3e74911145a92efe3c79ed5134853f
Zahhask <tomasgaspar95@gmail.com>
2025-08-13 18:51:36 +0100

    Fixed the thread_safe_iostream
    changed the operator<<(T&) for const T&

--- libftpp/inc/thread_safe_iostream.hpp
+++ libftpp/inc/thread_safe_iostream.hpp
@@ -2,49 +2,44 @@
 #define THREAD_SAFE_IOSTREAM_HPP
 
 #include <iostream>
-#include <thread>
 #include <mutex>
 #include <sstream>
+#include <thread>
 
-class ThreadSafeIOStream{
-	public:
-		void setPrefix(const std::string& prefix);
-		
-		template<typename T>
-		ThreadSafeIOStream& operator<<(T& p_prefix){
-			_buffer << p_prefix;
-			return *this;	
-		};
+class ThreadSafeIOStream {
+public:
+  void setPrefix(const std::string &prefix);
 
-		// This will handle the manipulators std::endl, std::flush, etc.
-		ThreadSafeIOStream& operator<<(std::ostream& (*manipulator)(std::ostream&));
+  template <typename T> ThreadSafeIOStream &operator<<(const T &p_prefix) {
+    _buffer << p_prefix;
+    return (*this);
+  };
 
-		template<typename T>
-		ThreadSafeIOStream &operator>>(T& obj){
-			const std::lock_guard<std::mutex> lock(_mutex);
-			std::cout << _thread_prefix;
-			
-			std::cin >> obj;
-			return (*this);
-		};
+  // This will handle the manipulators std::endl, std::flush, etc.
+  ThreadSafeIOStream &operator<<(std::ostream &(*manipulator)(std::ostream &));
 
+  template <typename T> ThreadSafeIOStream &operator>>(T &obj) {
+    const std::lock_guard<std::mutex> lock(_mutex);
+    std::cout << _thread_prefix;
 
-		
+    std::cin >> obj;
+    return (*this);
+  };
 
-		template<typename T>
-		void prompt(const std::string& question, T& dest){
-			const std::lock_guard<std::mutex> lock(_mutex);
-			std::cout << _thread_prefix;
+  template <typename T> void prompt(const std::string &question, T &dest) {
+    const std::lock_guard<std::mutex> lock(_mutex);
+    std::cout << _thread_prefix;
 
-			std::cout << question;
-			std::cin >> dest;
-		};
-	private:
-		static std::mutex _mutex;
-		std::string _thread_prefix;
-		std::ostringstream _buffer;
+    std::cout << question;
+    std::cin >> dest;
+  };
+
+private:
+  static std::mutex _mutex;
+  std::string _thread_prefix;
+  std::ostringstream _buffer;
 };
 
-extern thread_local ThreadSafeIOStream threadSafeCout; 
+extern thread_local ThreadSafeIOStream threadSafeCout;
 
 #endif
--- libftpp/src/thread_safe_iostream.cpp
+++ libftpp/src/thread_safe_iostream.cpp
@@ -3,20 +3,20 @@
 thread_local ThreadSafeIOStream threadSafeCout;
 std::mutex ThreadSafeIOStream::_mutex;
 
-ThreadSafeIOStream& ThreadSafeIOStream::operator<<(std::ostream& (*manipulator)(std::ostream&)){
-	const std::lock_guard<std::mutex> lock(_mutex);
-	std::ostringstream buffer;
+ThreadSafeIOStream &
+ThreadSafeIOStream::operator<<(std::ostream &(*manipulator)(std::ostream &)) {
+  const std::lock_guard<std::mutex> lock(_mutex);
+  std::ostringstream buffer;
 
-	buffer << _thread_prefix << _buffer.str() << manipulator;
+  buffer << _thread_prefix << _buffer.str() << manipulator;
 
-	std::cout << buffer.str();
-	_buffer.str("");
-	_buffer.clear();
-	return (*this);
+  std::cout << buffer.str();
+  _buffer.str("");
+  _buffer.clear();
+  return (*this);
 }
 
-void ThreadSafeIOStream::setPrefix(const std::string& prefix){
-	const std::lock_guard<std::mutex> lock(_mutex);
-	_thread_prefix = prefix;
-	
+void ThreadSafeIOStream::setPrefix(const std::string &prefix) {
+  const std::lock_guard<std::mutex> lock(_mutex);
+  _thread_prefix = prefix;
 }
