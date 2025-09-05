71dea2aa212a4f232e78512aa7fa36ebd656db08
Zahhask <tomasgaspar95@gmail.com>
2025-09-05 12:59:49 +0100

    Only client receving the remake

--- libftpp/inc/client.hpp
+++ libftpp/inc/client.hpp
@@ -2,6 +2,8 @@
 #define CLIENT_HPP
 
 #include "message.hpp"
+#include <atomic>
+#include <exception>
 #include <functional>
 #include <unistd.h>
 #include <cstring>
@@ -12,27 +14,33 @@
 
 class Client{
 	public:
-		Client(): _msgSize(0){};
+		using Action = std::function<void(const Message& msg)>;
+
+		Client();
+		~Client();
+
 		void connect(const std::string& address, const size_t& port);
 		void disconnect();
-		void defineAction(const Message::Type& messageType, const std::function<void(const Message& msg)>& action);
+
+		
 		void send(const Message& message);
+		void defineAction(const Message::Type& messageType, Action& action);
+		
 		void update();
 
-		// For the Server
-		int getSocket() const {return _socket;}
-		std::queue<Message *> *getMessages(){return &_msgQueue;}
-		Client(int socket): _msgSize(0), _socket(socket){}
-		bool receiveMsg(const std::string &buffer);
 	private:
-		size_t _msgSize;
-		std::string _buffer;
-		std::queue<Message *> _msgQueue;
-		// Components
-		int _socket;
-		struct sockaddr_in _serv_addr;
-		std::unordered_map<Message::Type, std::function<void(const Message&)>> _actions;
-	private:
+		int _sockfd;
+		std::atomic<bool> _isConnected;
+		std::unordered_map<Message::Type, Action> _actions;
+
+	public:
+		// Exceptions
+		class AlreadyConnectExceptions : public std::exception {
+			const char *what() const noexcept {return "Client already connected";};
+		};
+		class NoConnectExceptions : public std::exception {
+			const char *what() const noexcept {return "Client not connected";};
+		};
 };
 
 #endif
