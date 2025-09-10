6e917e0243112663668316d87c5cbe9289b148d4
Zahhask <tomasgaspar95@gmail.com>
2025-09-10 17:40:04 +0100

    Closes: #2 Fixed the leaks

--- libftpp/inc/pool.hpp
+++ libftpp/inc/pool.hpp
@@ -1,7 +1,10 @@
 #ifndef POOL_HPP
 #define POOL_HPP
 
-#include <iostream>
+#include <vector>
+#include <memory>
+#include <stdexcept>
+#include <algorithm>
 
 
 template<typename TType>
@@ -10,61 +13,109 @@ class Pool{
 		// nested class
 		class Object{
 			private:
-				friend class Pool<TType>;
-				Pool* pool;
-				TType* ptr;
-				size_t index;
+				friend class Pool;
+				
+				Pool<TType>* pool;
+				TType* obj;
 
-				Object(Pool *p, TType* obj, size_t i): pool(p), ptr(obj), index(i){}
+				Object(Pool<TType> *p_pool, TType* p_obj): pool(p_pool), obj(p_obj){}
+				
 			public:
 				~Object(){
-					ptr->~TType();
-					pool->used[index] = false;
+					obj->~TType();
+
+					if (pool){
+						if (obj)	
+							pool->_availObj.push_back(obj);
+					}
 				}
 				
 				TType* operator->(){
-					return ptr;
+					return obj;
 				};
 					
 		};
 
 
 	private:
-		TType *obj;
-		bool* used;
-		size_t cap;
+		std::vector<std::unique_ptr<TType>> _allocObj;
+		std::vector<TType*> _availObj;
+		size_t _total = 0;
+
 	public:
-	// resizing my obj capacity
-	void resize(const size_t& numberOfObjectsStored){
-		cap = numberOfObjectsStored;
-		obj = static_cast<TType*>(::operator new(sizeof(TType) * cap));
-		used = new bool[cap]();
-	};
+		void resize(const size_t& numberOfObjectsStored){
+			if (numberOfObjectsStored > _total){
+				// grow size
+				size_t objToAlloc = numberOfObjectsStored - _total;
 
-	
-	template<typename... TArgs>
-	Object acquire(TArgs&&... p_args){
-		for (size_t i = 0; i < cap; i++){
-			if (!used[i]){
-				TType* obj = new (&obj[i]) TType(std::forward<TArgs>(p_args)...);
-				used[i] = true;
-				return Object(this, obj, i);
+				while (objToAlloc > 0){
+					auto newObj = std::make_unique<TType>();
+
+					_availObj.push_back(newObj.get());
+					_allocObj.push_back(std::move(newObj));
+
+					_total++;
+					objToAlloc--;
+				}
+			}else if(numberOfObjectsStored < _total){
+				// shrink size
+				size_t objToRem = _total - numberOfObjectsStored;
+
+				while (objToRem > 0 && !_allocObj.empty()){
+					if (_availObj.empty())
+						NoAvailableObjToRemoveException();
+
+					TType *obj = _availObj.back();
+					_availObj.pop_back();
+
+					auto it = std::find_if(_allocObj.begin(), _allocObj.end(), [obj](const std::unique_ptr<TType> &ptr) { return ptr.get() == obj;});
+
+					if (it != _allocObj.end()){
+						_allocObj.erase(it);
+						_total--;
+						objToRem--;
+					}
+				}
+				if (objToRem > 0)
+					throw NoAllocatedObjectsToRemoveException();
 			}
+		};
+
+		
+		template<typename... TArgs>
+		Object acquire(TArgs&&... p_args){
+			if (_availObj.empty())
+				throw NoAvailableObjectsException();
+
+			TType *obj = _availObj.back();
+			_availObj.pop_back();
+
+			new (obj) TType(std::forward<TArgs>(p_args)...);
+
+			Object objectWrapper(this, obj);
+			return objectWrapper;
 		}
-		return Object(nullptr, nullptr, 0);
-	}
 
 
 	
 
-	Pool(): obj(nullptr), used(nullptr), cap(0){}
-	~Pool(){
-		for (size_t i = 0; i < cap; i++){
-			obj[i].~TType();
-		}
-		::operator delete(obj);
-		delete[] used;
-	}
+	public:
+		class NoAvailableObjectsException : public std::runtime_error{
+			public:
+				explicit NoAvailableObjectsException():
+					runtime_error("Pool: No Available Objects."){}
+		};
+
+		class NoAvailableObjToRemoveException : public std::runtime_error{
+			public:
+				explicit NoAvailableObjToRemoveException():
+					runtime_error("Pool: No Available Objects to Remove."){}
+		};
+
+		class NoAllocatedObjectsToRemoveException : public std::exception{
+			public:
+				const char *what() const noexcept {return "Pool: No Allocated Objects to Remove.";}
+		};
 	
 };
 
